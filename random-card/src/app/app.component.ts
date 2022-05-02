81043ae7e52aad3b33d0c60b86449aeba68a6934
Gaurav Sharma <gshobi3838@gmail.com>
2022-05-02 19:18:19 +0530

    developed random-card by using api

--- random-card/src/app/app.component.ts
+++ random-card/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'random-card';
+
+  user: any;
+
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService
+  ) {}
+  ngOnInit() {
+    this.userService.getUser().subscribe((user: any) => {
+        this.user = user.results[0];
+        console.log('user', user);
+        console.log('user', this.user);
+        
+    },
+    (err) => {
+      this.toastr.error(err.status, "Oops something wrong !!");
+    }
+    )
+  }
 }
